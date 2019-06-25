f8ce9c9994eaf313125330b972c9ab4e36fed7ee
Tommy Wilczk <tommywilczek@utexas.edu>
2019-06-25 18:56:15 -0500

    configuring travis

--- karma.conf.js
+++ karma.conf.js
@@ -25,13 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'], 
-    customLaunchers: {
-      ChromeHeadless: {
-          base: 'Chrome',
-          flags: ['--disable-translate', '--disable-extensions', '--remote-debugging-port=9223', '--no-sandbox']
-        }
-    },
+    browsers: ['Chrome'], 
+    // customLaunchers: {
+    //   ChromeHeadless: {
+    //       base: 'Chrome',
+    //       flags: ['--disable-translate', '--disable-extensions', '--remote-debugging-port=9223', '--no-sandbox']
+    //     }
+    // },
     singleRun: false,
     restartOnFileChange: true
   });
